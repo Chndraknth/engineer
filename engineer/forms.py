8dad400324887f489101efd854814ce2c1b3aa94
Siddhartha RT <sddhrthrt@thinkpad.(none)>
2012-09-23 11:29:39 +0530

    small work

--- engineer/forms.py
+++ engineer/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.shortcuts import get_object_or_404
 from engineer.database.models import *
 from django.contrib import admin
 from django.forms import widgets
@@ -7,11 +8,19 @@ class AddEventRegistrationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AddEventRegistrationForm, self).__init__(*args, **kwargs)
         print self.fields['events'].widget
-        self.fields['events'].widget = widgets.CheckboxSelectMultiple
-        print self
+        self.fields['events'].widget = widgets.CheckboxSelectMultiple(choices=self.fields['events'].widget.choices)
         
     class Meta:
         model = Participant
         fields = ('events', )
+
+class ProgressEventForm(forms.Form):
+    select_from = forms.MultipleChoiceField(
+            "Upgrade round",
+            widget = widgets.CheckboxSelectMultiple,
+            )
+
+    def __init__(self, p, e):
+        p = get_object_or_404(Participant, slug__iexact = p.slug)
 
 
--- engineer/views.py
+++ engineer/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
+from django.core import serializers
 from django.shortcuts import render_to_response, render, get_object_or_404
 from django.core.context_processors import csrf
 from engineer.database.models import * 
+from django.forms.models import model_to_dict
 from django.http import HttpResponseNotFound, HttpResponseRedirect, Http404
 from engineer.forms import *
 
@@ -15,7 +17,6 @@ def index(requet):
 def check_event_registration(request, slug=''):
     p = get_object_or_404(Participant.objects.filter(slug__iexact = slug))
     events = p.events.all()
-    jsonize(form)
     return render_to_response('check.html', locals())
 
 def add_event_registration(request, slug=''):
@@ -24,6 +25,7 @@ def add_event_registration(request, slug=''):
     p = get_object_or_404(Participant.objects.filter(slug__iexact = slug))
     if request.method == 'POST':
         form = AddEventRegistrationForm(request.POST, instance = p)
+        print request.POST
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/check/'+slug)
